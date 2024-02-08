325361d4e45c06191494fd637b829035de8770cf
miracle <prajinakk2002@gmail.com>
2024-02-08 19:44:11 +0900

    added index.html

--- customers/urls.py
+++ customers/urls.py
@@ -14,14 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+
+from django.urls import path
+from . views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('customers.urls')),
-    path('',include('products.urls')),
-    path('',include('orders.urls')),
-    path('',include('themes.urls'))
-
+        path('',)
+    
 ]
--- products/urls.py
+++ products/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+
+from django.conf import settings
+from django.urls import path
+from django.conf.urls.static import static
+from . views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('customers.urls')),
-    path('',include('products.urls')),
-    path('',include('orders.urls')),
-    path('',include('themes.urls'))
-
+        path('',index,name='index')
+    
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
